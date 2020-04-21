383995cfae5970c75d73d3d02762e81a20b2b7de
Logan McGuire <lrmcguire93@gmail.com>
2020-04-21 17:33:17 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,5 +1,8 @@
 def using_concat (array1, array2)
   concated_array = [];
-  
+  array2.length.times do |counter|
+    concated_array = array1 << array2[counter];
+  end
+  return concated_array;
 end
 
